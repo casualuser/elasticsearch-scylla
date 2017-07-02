631badddb4db587de36b2384c2794c0a66e4ba4d
Phillip Tribble <phillip.tribble@gmail.com>
2017-07-02 09:20:12 -0700

    clean code

--- twitter/createKeyspace.js
+++ twitter/createKeyspace.js
@@ -20,14 +20,14 @@ var populateData = function(date, username, tweet, url) {
     contactPoints: [scylladb_server],
     keyspace: 'fluentdloggers'
   });
-//tweet = clean.escape(tweet);
-//url = clean.escape(url);
-//date = clean.escape(date);
-//username = clean.escape(username);
-var query = 'INSERT INTO tweets (date,username,tweet, url) VALUES (?, ?, ?, ?)';
-const parms = [date, username, tweet, url];
+  //tweet = clean.escape(tweet);
+  //url = clean.escape(url);
+  //date = clean.escape(date);
+  //username = clean.escape(username);
+  var query = 'INSERT INTO tweets (date,username,tweet, url) VALUES (?, ?, ?, ?)';
+  const parms = [date, username, tweet, url];
 
-  client.execute(query, parms,function(err, result) {
+  client.execute(query, parms, function(err, result) {
 
     if (err) {
       console.log('\nError' + err);
